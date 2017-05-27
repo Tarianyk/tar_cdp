caec3b99e9e512a5145deed9ac2f3f69f908de04
Anton Tarianyk <anton_tarianyk@epam.com>
2017-05-27 12:11:15 +0300

    init commit

---
+++ M1/Hw1/cdp-git/src/main/java/com/epam/m1/hw1/first/First.java
@@ -0,0 +1,13 @@
+package com.epam.m1.hw1.first;
+
+public class First {
+
+    public String methodOne() {
+        return "c1m1";
+    }
+	
+	public String methodTwo() {
+        return "c1m2";
+    }
+
+}
---
+++ M1/Hw1/cdp-git/src/main/java/com/epam/m1/hw1/second/Second.java
@@ -0,0 +1,13 @@
+package com.epam.m1.hw1.second;
+
+public class Second {
+
+    public String methodOne() {
+        return "c2m1";
+    }
+	
+	public String methodTwo() {
+        return "c2m2";
+    }
+
+}
